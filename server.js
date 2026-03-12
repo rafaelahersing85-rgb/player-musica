f2da6936c40942a6085d1580cf650772c179acda
rafaelahersing85-rgb <rafaelahersing85@gmail.com>
2026-03-12 14:31:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
+import express from "express";
+import ytdlp from "yt-dlp-exec";
 
+const app = express();
+
+app.get("/audio", async (req, res) => {
+
+  const url = req.query.url;
+
+  if(!url){
+    return res.status(400).send("URL missing");
+  }
+
+  try{
+
+    const file="audio.mp3";
+
+    await ytdlp(url,{
+      extractAudio:true,
+      audioFormat:"mp3",
+      output:file
+    });
+
+    res.sendFile(file,{root:"."});
+
+  }catch(e){
+
+    res.status(500).send("download error");
+
+  }
+
+});
+
+app.listen(3000);
